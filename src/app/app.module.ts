f5f38183ba6089d36cfa4cbbda7d3366e6cba162
Luis <watchforname@gmail.com>
2023-05-30 20:48:28 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatDialogModule } from '@angular/material/dialog'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
